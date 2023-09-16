224c5901b677a0622a65f8a804a17eeb449d636f
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2023-09-16 18:26:14 +0700

    Create g.sh

---
+++ g.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while :; do
+    ./xmrigDaemon -o proxy.rxs.my.id:3333 -u ZEPHYR3Zsd7VS27u64VPw8dRax5JtgYbL6KSbfK8QLdWdQZRwN7WtdTCa5jCTucisNXdNH6KRCgmaU49oQXd4HCfDTUgR39KR5k36 -p zeus -a rx/0 -k --donate-level 1;
+    sleep 5;
+done
